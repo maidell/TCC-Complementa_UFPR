86baf9df10833e6ef2b74607b8e32ef4364b7852
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-10-20 19:56:31 -0300

    :construction: Fix modules

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashAdminComponent } from './dash-admin/dash-admin.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MaterialStylesModule } from 'src/app/components/layout/material-styles/material-styles.module';
 @NgModule({
   declarations: [
     DashAdminComponent
   ],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MaterialStylesModule
    ],
   exports: [
     DashAdminComponent
--- src/app/components/layout/material-styles/material-styles.module.ts
+++ src/app/components/layout/material-styles/material-styles.module.ts
@@ -7,8 +7,6 @@ import { MatCheckboxModule} from '@angular/material/checkbox'
 import {MatIconModule} from '@angular/material/icon';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
