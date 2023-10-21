f8dce56f4ad0bc40869b01ec031290ff95d4402a
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-10-21 14:59:10 -0300

    :warning: Fix material imports

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutModule } from '../layout/layout.module';
-import { MaterialStylesModule } from 'src/material-styles/material-styles.module';
+import { MaterialStylesModule } from 'src/app/components/layout/material-styles/material-styles.module';
 
 @NgModule({
   declarations: [
