558f272ed88687f2ee4dabafd8522d6ddd2bd068
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-10-27 18:08:29 -0300

    :construction_worker: WIP: cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { DashAdminComponent } from './components/admin/dash-admin/dash-admin.com
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'aluno/home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
