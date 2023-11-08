060abc6bf3673b96658885dbe8b43c04bd18d774
leohortmann <leonardohortmann03@gmail.com>
2023-11-07 23:47:47 -0300

    alterações na responsividade e no conteúdo do header de atividades. Responsividade ta quase pronta

--- src/app/components/atividade/atividade/atividade.component.ts
+++ src/app/components/atividade/atividade/atividade.component.ts
@@ -6,8 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./atividade.component.scss']
 })
 export class AtividadeComponent {
-  estado: string ='Execução Contestada';
+  estado: string ='Finalizada'; // Nova, Aberta, Em Execução, Carga Horária Contestada, Execução Contestada, Finalizada
+  statusButtonColor='';
 
+  firstButtonColor='';
+  firstHeaderButton = '';
+  
+  secondButtonColor='';
+  secondHeaderButton = '';
+
+  thirdButtonColor='';
+  thirdHeaderButton = '';
+
+  newActivityDisplay = '';
+  
+
+  ngOnInit(){
+    this.setHeaderButtons();
+  }
+
+  setHeaderButtons(){
+    switch (this.estado){
+      case 'Nova':  // tela de criação de atividades
+        this.secondHeaderButton='Salvar';
+        this.thirdHeaderButton='Cancelar';
+        this.newActivityDisplay='none';
+        this.statusButtonColor='linear-gradient(#3473a3,#5b7ba5)';
+        this.secondButtonColor='linear-gradient(#559958, #418856)';
+        this.thirdButtonColor='linear-gradient(#C7433F, #C7241F)';
+        break;
+      case 'Aberta':  // para aluno solicitante. Tratar possibilidades pro aluno executor
+        this.firstHeaderButton='Visualizar Candidaturas';
+        this.secondHeaderButton='Editar';
+        this.thirdHeaderButton='Fechar';
+        this.firstButtonColor='linear-gradient(#2494D3,#0076D0)';
+        this.secondButtonColor='linear-gradient(#CC6E00,#D95409)';
+        this.thirdButtonColor='linear-gradient(#C7433F, #C7241F)';
+        break;
+      case 'Em Execução': //visão do solicitante quando o relatorio de conclusão tiver sido preenchido. tratar quando não tiver sido preenchido ainda
+        this.firstHeaderButton='Finalizar';
+        this.secondHeaderButton='Contestar';
+        this.thirdHeaderButton='Fechar';
+        this.firstButtonColor='linear-gradient(#2494D3,#0076D0)';
+        this.secondButtonColor='linear-gradient(#CC6E00,#D95409)';
+        this.thirdButtonColor='linear-gradient(#C7433F, #C7241F)';
+        break;
+      case 'Carga Horária Contestada':
+        
+        break;
+      case 'Execução Contestada':
+
+        break;
+      case 'Finalizada':
+        this.secondHeaderButton='Gerar Certificado';
+        this.thirdHeaderButton='Fechar';
+        this.newActivityDisplay='none';
+        this.statusButtonColor='linear-gradient(#318B35, #297E42)';
+        this.secondButtonColor='linear-gradient(#559958, #418856)';
+        this.thirdButtonColor='linear-gradient(#C7433F, #C7241F)';
+        break;
+    }
+  }
 
 
 }
