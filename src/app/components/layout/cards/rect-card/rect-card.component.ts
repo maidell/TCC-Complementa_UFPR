34c6c489d94151ce2b039fd899e68ead1e8b0966
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-11-19 14:46:25 -0300

    :construction: Fix responsivity

--- src/app/components/layout/cards/rect-card/rect-card.component.ts
+++ src/app/components/layout/cards/rect-card/rect-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-rect-card',
   templateUrl: './rect-card.component.html',
-  styleUrls: ['./rect-card.component.scss', './styles.scss']
+  styleUrls: ['./rect-card.component.scss']
 })
 export class RectCardComponent{
   @Input() nome!: string;
