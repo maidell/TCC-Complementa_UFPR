f0af9ed015dc2e72981862678b21e79a67265e6a
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-11-19 21:49:48 -0300

    :construction: WIP: table gerencias

---
+++ src/app/components/layout/cards/table-card/table-card.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-table-card',
+  templateUrl: './table-card.component.html',
+  styleUrls: ['./table-card.component.scss']
+})
+export class TableCardComponent<T> implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.displayedColumns = [];
+    for (let column of this.columns) {
+      this.displayedColumns.push(column.key);
+    }
+    this.displayedColumns.push('button');
+    console.log(this.displayedColumns);
+    console.log(this.dataSource);
+
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  @Input() dataSource!: MatTableDataSource<T>;
+  @Input() button!: string;
+  @Input() buttonColor?: string;
+  @Input() columns!: {title: string, suffix?: string, key: string}[];
+  @Input() display!: string;
+  displayedColumns!: string[];
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+}
--- src/app/components/layout/competencias/competencias.component.ts
+++ src/app/components/layout/competencias/competencias.component.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from '../../title.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Competencia } from 'src/app/shared';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-competencias',
   templateUrl: './competencias.component.html',
   styleUrls: ['./competencias.component.scss']
 })
-export class CompetenciasComponent implements OnInit{
-  constructor(private titleService: TitleService) { }
+export class CompetenciasComponent implements OnInit {
+  button: string = "Detalhes!";
+  constructor(private titleService: TitleService, public dialog: MatDialog) {
+    this.dataSource = new MatTableDataSource<Competencia>(this.competencias);
+  }
+  columns: { title: string, key: string }[] = [
+    { title: "Competência", key: 'nome' }
+  ];
 
+  dataSource!: MatTableDataSource<Competencia>;
   ngOnInit(): void {
     this.titleService.setTitle('Competencias');
+    this.dataSource = new MatTableDataSource<Competencia>(this.competencias);
   }
-  objections: String[] = ["aaaa", "bbbb", "cccc"];
+  competencias: Competencia[] = [
+    { nome: 'Front-end', id: 2304 },
+    { nome: 'Back-end', id: 2305 },
+    { nome: 'Full-stack', id: 2306 },
+    { nome: 'Mobile', id: 2307 },
+    { nome: 'DevOps', id: 2308 },
+    { nome: 'UX/UI', id: 2309 },
+    { nome: 'Data Science', id: 2310 },
+    { nome: 'Business Intelligence', id: 2311 },
+    { nome: 'Business Analytics', id: 2312 },
+    { nome: 'Outros', id: 2313 },
+  ]
 
-  hasObjections(): boolean {
-    return this.objections.length > 0;
+  hasObjects(): boolean {
+    return this.competencias.length > 0;
   }
 
 }
