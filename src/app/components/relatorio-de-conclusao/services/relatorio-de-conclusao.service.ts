865e498cd40ad00de240f64345716d696b6a06f6
GuiborLukas <93823570+GuiborLukas@users.noreply.github.com>
2023-11-23 16:37:26 -0300

    Correção do service de relatoriod e conclusao

--- src/app/components/relatorio-de-conclusao/services/relatorio-de-conclusao.service.ts
+++ src/app/components/relatorio-de-conclusao/services/relatorio-de-conclusao.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ConclusaoService {
+export class RelatorioDeConclusaoService {
 
   constructor() { }
 }
