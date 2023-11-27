da25749cc255b41fb092245189521d519ac976b6
SaintGuibor <93823570+GuiborLukas@users.noreply.github.com>
2023-11-27 17:06:34 -0300

    Alterações para binding da listagem de atividades

--- server/complementa-ufpr/src/main/java/br/ufpr/repository/AtividadeRepository.java
+++ server/complementa-ufpr/src/main/java/br/ufpr/repository/AtividadeRepository.java
@@ -14,21 +14,21 @@ import br.ufpr.model.Projeto;
 import br.ufpr.model.Usuario;
 
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
-	
-	Optional<Atividade> findById(String id);
-	
-	List<Atividade> findAllByProjeto(Projeto projeto);
-	
-	List<Atividade> findAllByExecutor(Aluno executor);
-	
-	List<Atividade> findAllByAutor(Usuario autor);
-	
-    public long count();
-    
-    @Query("SELECT a FROM Atividade a " +
-            "JOIN a.graduacoes g " +
-            "WHERE g = :graduacao")
-    List<Atividade> findAllByGraduacao(@Param("graduacao") Graduacao graduacao);
 
+	Optional<Atividade> findById(String id);
+
+	List<Atividade> findAllByProjeto(Projeto projeto);
+
+	List<Atividade> findAllByExecutor(Aluno executor);
+
+	List<Atividade> findAllByAutor(Usuario autor);
+
+	public long count();
+
+	@Query("SELECT a FROM Atividade a " + "JOIN a.graduacoes g " + "WHERE g = :graduacao")
+	List<Atividade> findAllByGraduacao(@Param("graduacao") Graduacao graduacao);
+
+	@Query("SELECT a FROM Atividade a WHERE a.projeto.orientador.id = :orientadorId")
+	List<Atividade> findAllByOrientadorId(@Param("orientadorId") Long orientadorId);
 
 }
