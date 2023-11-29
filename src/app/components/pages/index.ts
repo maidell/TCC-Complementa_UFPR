a97a23660d5c7b0d16e61104ac67bc2d9a94f574
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-11-29 04:14:57 -0300

    :rocket: Validar certificado is done!

--- src/app/components/pages/index.ts
+++ src/app/components/pages/index.ts
@@ -8,4 +8,10 @@ export * from './meu-perfil/meu-perfil.component';
 export * from './projetos/projetos.component';
 export * from './servidores/servidores.component';
 export * from './competencias/competencias.component';
+export * from './autocadastro/autocadastro.component';
+export * from './editar-aluno/editar-aluno.component';
+export * from './cadastro-de-usuarios/cadastro-de-usuarios.component';
+export * from './confirmacao/confirmacao.component';
+export * from './editar-usuarios/editar-usuarios.component';
+export * from './valida-certificado/valida-certificado.component';
 
---
+++ src/app/components/pages/valida-certificado/valida-certificado.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CertificadoService } from 'src/app/services/certificado/services/certificado.service';
+import { TitleService } from 'src/app/services/title/title.service';
+import { Certificado } from 'src/app/shared';
+
+@Component({
+  selector: 'app-valida-certificado',
+  templateUrl: './valida-certificado.component.html',
+  styleUrls: ['./valida-certificado.component.scss']
+})
+export class ValidaCertificadoComponent implements OnInit{
+  ngOnInit(): void {
+    this.title.setTitle('Validar certificado');
+  }
+
+  hash: string = '';
+  certificado: Certificado | null = null;
+  validacaoStatus: string = '';
+
+  constructor(
+    private certificadoService: CertificadoService,
+    private title: TitleService,
+    ) { }
+
+  validarCertificado() {
+    if (this.hash.trim() !== '') {
+      this.certificadoService.buscarCertificadoPorHash(this.hash.trim())
+        .subscribe(
+          (certificado: Certificado) => {
+            if (certificado && certificado.id) {
+              this.certificado = certificado;
+              this.validacaoStatus = 'Certificado válido!';
+            } else {
+              this.validacaoStatus = 'Certificado inválido!';
+              this.certificado = null;
+            }
+          },
+          (error) => {
+            this.validacaoStatus = 'Erro ao buscar certificado.';
+            this.certificado = null;
+          }
+        );
+    } else {
+      this.validacaoStatus = 'Por favor, insira um hash válido!';
+      this.certificado = null;
+    }
+  }
+}
