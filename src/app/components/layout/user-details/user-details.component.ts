200bf4609fffe21677f75c1bd0828bcee5dae8be
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-11-30 13:16:12 -0300

    :rocket: user detail is done!

--- src/app/components/layout/user-details/user-details.component.ts
+++ src/app/components/layout/user-details/user-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../auth/services/login.service';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/shared';
 
 @Component({
   selector: 'app-user-details',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
 export class UserDetailsComponent {
   exibir: boolean = true;
   userName = "";
+  usuarioLogado: Usuario = new Usuario();
+
   grr = "";
   name = this.userName.split(/[, ]+/);
   displayName = '';
@@ -22,21 +25,23 @@ export class UserDetailsComponent {
   ngOnInit(): void {
    if (this.loginService.usuarioLogado) {
       this.exibir = true;
+     this.usuarioLogado = this.loginService.usuarioLogado;
       this.userName = this.loginService.usuarioLogado.nome;
-      this.grr = this.loginService.usuarioLogado.papel;
+      this.grr = this.loginService.usuarioLogado.nome;
       this.printName();
     } else {
       this.loginService.usuarioLogado$.subscribe(usuario => {
         if (usuario) {
           this.exibir = true;
           this.userName = this.loginService.usuarioLogado.nome;
-          this.grr = this.loginService.usuarioLogado.papel;
+          this.grr = this.loginService.usuarioLogado.nome;
           this.printName();
         } else {
           this.exibir = false;
         }
       });
     }
+
   }
 
   printName() {
